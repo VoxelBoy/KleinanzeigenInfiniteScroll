10f89569a6021ddc87461e1f6944c2d5790d5d5e
Yilmaz Kiymaz <ylmzkymz@gmail.com>
2023-08-16 22:13:38 +0200

    Improve grid layout for readability

--- Package/KleinanzeigenUserScript.js
+++ Package/KleinanzeigenUserScript.js
@@ -39,6 +39,7 @@ modalStyle.innerHTML = `
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
     gap: 20px;
+    align-items: end;
   }
 `;
 document.head.appendChild(modalStyle);
